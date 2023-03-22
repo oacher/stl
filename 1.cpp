58bc39e6ae42bfc696bd35a139af08ab6ca9c5eb
oacher <ochierniatieva@mail.ru>
2023-03-22 14:42:54 +0300

    first commit

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <set>
+
+int main() {
+    
+    int n;
+    std::cin >> n;
+
+    std::set<int> uniqueNumbers;
+
+    for (int i = 0; i < n; i++) {
+        int num;
+        std::cin >> num;
+        uniqueNumbers.insert(num);
+    }
+
+    std::cout <<  uniqueNumbers.size() << std::endl;
+
+    return 0;
+}
---
+++ 2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <stack>
+
+int main() {
+    std::string s;
+    std::cin >> s;
+
+    std::stack<char> st;
+    for (char c : s) {
+        if (c == '(' || c == '[' || c == '{') {
+            st.push(c);
+        }
+        else if (c == ')' || c == ']' || c == '}') {
+            if (st.empty()) {
+                std::cout << "no" << std::endl;
+                return 0;
+            }
+            char last = st.top();
+            st.pop();
+            if ((c == ')' && last != '(') ||
+                (c == ']' && last != '[') ||
+                (c == '}' && last != '{')) {
+                std::cout << "no" << std::endl;
+                return 0;
+            }
+        }
+    }
+
+    if (st.empty()) {
+        std::cout << "yes" << std::endl;
+    }
+    else {
+        std::cout << "no" << std::endl;
+    }
+
+    return 0;
+}
